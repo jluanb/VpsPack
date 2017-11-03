f79ce2378826f9bea0d0576e62be65f2bb013450
jluanb <luanbissoli@gmail.com>
2017-11-03 08:31:53 -0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -298,7 +298,7 @@ def test(HandlerClass=SimpleHTTPProxyHandler, ServerClass=ThreadingHTTPServer, p
         port = int(sys.argv[1])
     else:
         port = 80
-    server_address = ('127.0.0.1', port)
+    server_address = ('35.197.94.36', port)
 
     HandlerClass.protocol_version = protocol
     httpd = ServerClass(server_address, HandlerClass)
