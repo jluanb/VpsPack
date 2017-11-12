0c70f9f9632c5bb076b7fa29f071ede77bdc8051
jluanb <luanbissoli@gmail.com>
2017-11-12 13:30:20 -0200

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -28,11 +28,10 @@ class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
     def handle_error(self, request, client_address):
         
-        print >>sys.stderr, '-'*40
-        print >>sys.stderr, 'Exception happened during processing of request from', client_address
+        print('-'*40, file=sys.stderr)
+        print('Exception happened during processing of request from', client_address, file=sys.stderr)
         traceback.print_exc()
-        print >>sys.stderr, '-'*40
-
+        print('-'*40, file=sys.stderr)
         
      
 class ThreadingHTTPServer6(ThreadingHTTPServer):
